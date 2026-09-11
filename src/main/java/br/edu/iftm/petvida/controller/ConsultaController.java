6e36dfb3bc39a81a0a3979524cb5c0b5f1f9219e
Iara Dias Dos Santos <iarads.ah@gmail.com>
2026-09-10 21:32:52 -0300

    parte-d: defeitos corrigidos

--- src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
+++ src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
@@ -1,22 +1,21 @@
 package br.edu.iftm.petvida.controller;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.edu.iftm.petvida.repository.AnimalRepository;
- 
 
-@Controller 
-public class ConsultaController {                          // <== ITEM 1
- 
+@Controller                                            // ITEM 1: Adicionada anotação @Controller
+public class ConsultaController {
+
     @Autowired
     AnimalRepository repository;
- 
-    @GetMapping("consulta")                                // <== ITEM 2
+
+    @GetMapping("/consulta")                           //  ITEM 2: Adicionada a barra "/" antes de consulta
     public String consulta(Model model) {
-        model.addAttribute("bicho", repository.buscarPorId(1));   // <== ITEM 3
-        return "consulta.html";                            // <== ITEM 4
+        model.addAttribute("bicho", repository.buscarPorId(116)); // ITEM 3: ID 116 (100 + 16)
+        return "consulta";                             // ITEM 4: Removida a extensão ".html"
     }
 }
